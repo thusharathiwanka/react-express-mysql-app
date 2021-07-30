9bd04c43db3ac804b9f37d28cd8075b87c09271f
Thushara Thiwanka <thusharathiwanka123@gmail.com>
2021-07-30 14:23:11 +0530

    CREATE: db connection created

--- server/server.js
+++ server/server.js
@@ -1,11 +1,25 @@
 const express = require("express");
-
+const mysql = require("mysql");
 const app = express();
 
-app.get("/", (req, res) => {
-	res.send("Root route is working");
-});
+mysql
+	.createConnection({
+		user: "root",
+		host: "localhost",
+		password: "",
+		database: "employee_system",
+	})
+	.connect((err) => {
+		if (err) {
+			return console.log("something went wrong. db not connected");
+		}
 
-app.listen(3001, () => {
-	console.log("Server is running on PORT 3001");
+		console.log("db connected");
+		app.listen(3001, () => {
+			console.log("Server is running on PORT 3001");
+		});
+	});
+
+app.get("/", (req, res) => {
+	res.send("Employee Management System");
 });
